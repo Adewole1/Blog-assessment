0f10445672334eaa9eb595c465625cadd666ea58
Adewole1 <madedayo@gmail.com>
2022-09-01 01:49:48 +0100

    Add test

--- Backend/blog/posts/tests.py
+++ Backend/blog/posts/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import BlogPost
+
+
+class BlogTests(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        testuser1 = User.objects.create_user(
+            username='testuser1', password='abc123'
+        )
+        testuser1.save()
+
+        test_post = BlogPost.objects.create(
+            author=testuser1, title='Test title', body='Test body content....'
+        )
+        test_post.save()
+
+    def test_blog_content(self):
+        post = BlogPost.objects.get(id=1)
+        author = f'{post.author}'
+        title = f'{post.title}'
+        body = f'{post.body}'
+        self.assertEqual(author, 'testuser1')
+        self.assertEqual(title, 'Test title')
+        self.assertEqual(body, 'Test body content....')
